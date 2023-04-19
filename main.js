19996c1a2674e94da6d2313fe64ba79e2efc19a0
isavalenzuela <isadoravalenzuelafierro@gmail.com>
2023-04-18 22:29:45 -0400

    add: asset images

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'IT Socks',
+      productDescription: 'They suck',
+      productImage: './assets/images/socks_chicken.webp',
+    }
+  },
+})
